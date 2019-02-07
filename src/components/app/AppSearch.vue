b2c5c9566dad1da39fd50f52148e608816c744d7
Oskar Rainer <rainer.oskar@gmail.com>
2019-02-07 11:11:20 +0100

    rm plceholder and add listeners

--- src/components/app/AppSearch.vue
+++ src/components/app/AppSearch.vue
@@ -3,9 +3,9 @@
         spellcheck="false"
         @submit.prevent="formSubmitted">
     <BaseInput v-model="keyword"
-               :placeholder="placeholder"
                required="true"
-               minlength="3" />
+               minlength="3"
+               v-on="$listeners" />
   </form>
 </template>
 
@@ -15,8 +15,6 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class AppSearch extends Vue {
   private keyword = ``
-
-  private placeholder = `Placeholder`
 
   private formSubmitted() {
     this.$router.push({
